6a1b2450a3bd2c643747095bdb552677e9223ad0
my0sot1s <manhte231@gmail.com>
2017-09-10 21:37:31 +0700

    fix clock

--- js/jswindow.js
+++ js/jswindow.js
@@ -34,3 +34,48 @@ blog.onclick = blogFn;
 home.onclick = homeFn;
 contact.onclick = contactFn;
 skills.onclick = skillsFn;
+
+function rand(num) {
+  return Math.floor(Math.random() * num) + 0;
+}
+var mang1 =
+  ['bigstock-desktop-mix-on-a.jpg'
+    , 'office-desk-background-2.jpg'
+    , 'banner3.jpg'
+    , 'banner2.jpg'
+    , 'banner1.jpg'
+  ],//
+  mang2 = [
+    'office-supplies-with-computer-on-white-desk_1357-241.jpg'
+    , 'home_bg.jpg'
+    , 'paper-pencil-work-touch-business-usb_1421-592.jpg'
+    , 'frame-with-office-equipment-on-white-desk_1357-235.jpg'
+  ]
+document.getElementsByTagName("body")[0].style
+  = "background-image: url('img/"
+  + mang1[rand(mang1.length)] + "');"
+document.body.onmouseenter = function () {
+  setTimeout(function () {
+    document.getElementsByTagName("body")[0].style =
+      "background-image: url('img/"
+      + mang2[rand(mang2.length)] + "');"
+  }, 500);
+}
+
+// lịch và đòng hồ
+
+var lich = document.getElementsByClassName('right_bar')[0];
+
+var _d = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+lich.insertAdjacentHTML("beforeend", '<h2>' + days[_d.getDay()] + '</h2>')
+lich.insertAdjacentHTML("beforeend", '<h2>' + _d.getUTCMonth() + '</h2>')
+lich.insertAdjacentHTML("beforeend", '<h2>' + _d.getFullYear() + '</h2>')
+
+//dong ho
+var dh = document.getElementsByClassName('right_clock')[0];
+setInterval(function () {
+  dh.innerHTML = ''; var str;
+  str = '<h2>' + _d.getHours() + ":" + _d.getMinutes() + '</h2>'
+  dh.insertAdjacentHTML("beforeend", str);
+}, 500);
